0db81086549908133f12bd2f46d6d47091a6d097
Gustavo Mauricio <gmauricio207@gmail.com>
2022-02-05 19:01:06 -0300

    Corrigindo style da pagina de atualizaçao

--- src/pages/UpdateCustumer.js
+++ src/pages/UpdateCustumer.js
@@ -1,93 +1,113 @@
-import React, {useEffect, useState, } from 'react';
-import { useParams } from 'react-router-dom'
-import api from '../services/api';
-// import sucessMenssage from '../componets/registerSucess';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../services/api";
+import Button from "@material-ui/core/Button";
+
+import '../styles/register.css'
 
 export default function UpdateCustumer() {
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
 
-    //useParams é para pegar o Id que foi passado por paramentro
-    const { id } = useParams();
+  //useParams é para pegar o Id que foi passado por paramentro
+  const { id } = useParams();
 
-    useEffect(() => {
-      async function getCustumer() {
-         const result = await api.get(`/custumers/${id}`);
-// console.log('RESULT',result);
-        setName(result.data.name)
-        setAge(result.data.age)
-        setCity(result.data.city)
-        setState(result.data.state)
-         
-      }
-      getCustumer()
-    }, [])
+  useEffect(() => {
+    async function getCustumer() {
+      const result = await api.get(`/custumers/${id}`);
+      // console.log('RESULT',result);
+      setName(result.data.name);
+      setAge(result.data.age);
+      setCity(result.data.city);
+      setState(result.data.state);
+    }
+    getCustumer();
+  }, []);
 
-    async function handleSubmit(){
-        const data = {name, age, city, state, id}
-        console.log(data);
-        const { status } = await api.put(`/custumers/${id}`, data)
-        console.log(status);
-        if (status === 200){
-            window.location.href='/getallcustumers'
-        } else {
-            alert('Erro ao atualizar o Cliente')
-        }
-    };
+  async function handleSubmit() {
+    const data = { name, age, city, state, id };
+    console.log(data);
+    const { status } = await api.put(`/custumers/${id}`, data);
+    console.log(status);
+    if (status === 200) {
+      window.location.href = "/getallcustumers";
+    } else {
+      alert("Erro ao atualizar o Cliente");
+    }
+  }
 
-    return(
-        <div>
-            <div>
-                <h2>Atualizar de Cadastro</h2>
-            </div>
-            <form>
-                <label htmlFor='name'>
-                    Nome:
-                    <input 
-                      type="text"
-                      name="name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      id='name'
-                      placeholder='Nome'
-                    />  
-                </label>
-                <label htmlFor='age'>
-                    Idade:
-                    <input 
-                      type="text"
-                      name='age'
-                      id='age'
-                      value={age}
-                      onChange={e => setAge(e.target.value)}
-                    />  
-                </label>
-                <label htmlFor='city'>
-                    Cidade:
-                    <input 
-                      type="text"
-                      name='city'
-                      id='city'
-                      placeholder='Cidade'
-                      value={city}
-                      onChange={e => setCity(e.target.value)}
-                    />  
-                </label>
-                <label htmlFor='state'>
-                    Estado:
-                    <input 
-                      type="text"                  
-                      id='state'
-                      placeholder='Estado'
-                      name='state'
-                      value={state}
-                      onChange={e => setState(e.target.value)}
-                    />  
-                </label>
-               <button type='button' onClick={handleSubmit}>Atualizar</button>
-            </form>
+  return (
+    <div className="container">
+      <div className="container-interno">
+        <div className="conteiner-text">
+          <h2>Atualizar de Cadastro</h2>
         </div>
-    )
+
+        <form>
+          <div className="wrapper">
+            <div className="input-single">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="name"
+								className="input"              
+              />
+              <label htmlFor="name">Nome</label>
+            </div>
+
+            <div className="input-single">
+              <input
+                type="text"
+                name="age"
+                id="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+								className="input"
+              />
+              <label htmlFor="age">Idade</label>
+            </div>
+
+            <div className="input-single">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                className="input"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <label htmlFor="city">Cidade:</label>
+            </div>
+
+            <div className="input-single">
+              <input
+                type="text"
+                id="state"
+                className="input"
+                name="state"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+              <label htmlFor="state">Estado</label>
+            </div>
+            <div className="button-container">
+              <div className="button1">
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={handleSubmit}
+                >
+                  Atualizar
+                </Button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
